667becce21c4ef5b59a071e4b4e53d55be26cf6f
Maanush007 <72811699+Maanush007@users.noreply.github.com>
2021-08-06 20:03:59 +0530

    Add files via upload

---
+++ js/Quiz.js
@@ -0,0 +1,76 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+    //write code here to hide question elements
+    question.hide();
+
+    //write code to change the background color here
+    background("yellow")
+
+    //write code to show a heading for showing the result of Quiz
+    fill(0);
+    textSize(30);
+    text("result of the quiz",340,50);
+    text("----------------------------",320,65);
+
+    //call getContestantInfo( ) here
+    Contestant.getPlayerInfo();
+
+    //write condition to check if contestantInfor is not undefined
+    if(allContestants!==undefined){
+      fill(0);
+      textSize(20);
+      text("*Note:Contestant Who Answered Correct are Highlighted in Green Color",130,230);
+      var displayPosition=230
+
+      for(var plr in allContestants){
+        var correctAns="2";
+        displayPosition+=30
+        if(correctAns===allContestants[plr].answer){
+          fill("green");
+          textSize(20);
+          text(allContestants[plr].name+": "+allContestants[plr].answer,250,displayPosition)
+        }
+        else{
+          fill("red");
+          textSize(20);
+          text(allContestants[plr].name+": "+allContestants[plr].answer,250,displayPosition)
+        }
+      }
+    }
+
+    //write code to add a note here
+
+    //write code to highlight contest who answered correctly
+    
+  }
+
+}
